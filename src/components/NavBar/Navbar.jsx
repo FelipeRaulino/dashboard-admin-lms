a33a4656168bd8aa39de79ff607ed862d4218dd4
Felipe Raulino <felipe.raaulino29@gmail.com>
2023-11-15 15:25:01 -0300

    feat: finalizando menu dropdown de notificações

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,12 +4,16 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import PaidIcon from "@mui/icons-material/Paid";
 
 import "./Navbar.css";
 import { Avatar } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   function onClickShowNavbarMenu() {
     const navbarMenu = document.getElementsByClassName(
       "navbar__menu-overlay-hidden"
@@ -26,6 +30,48 @@ const Navbar = () => {
     navbarMenu[0].classList.remove("navbar__menu-overlay");
   }
 
+  function onClickShowNotificationMenu(event) {
+    const notificationMenu = document.getElementById(
+      "navbar__notification-modal"
+    );
+
+    if (notificationMenu.classList.contains("menu-hidden")) {
+      notificationMenu.classList.remove("menu-hidden");
+    } else {
+      notificationMenu.classList.add("menu-hidden");
+    }
+
+    event.stopPropagation();
+  }
+
+  function handleOnClickOutsideNotificationMenu() {
+    const notificationMenu = document.getElementById(
+      "navbar__notification-modal"
+    );
+
+    document.addEventListener("click", function (event) {
+      if (notificationMenu) {
+        if (
+          !notificationMenu.contains(event.target) &&
+          event.target.id !== "navbar__notification-icon"
+        ) {
+          notificationMenu.classList.add("menu-hidden");
+        }
+      }
+    });
+  }
+
+  React.useEffect(() => {
+    handleOnClickOutsideNotificationMenu();
+
+    return () => {
+      document.removeEventListener(
+        "click",
+        handleOnClickOutsideNotificationMenu
+      );
+    };
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -33,17 +79,61 @@ const Navbar = () => {
         <span>Useradmin</span>
       </div>
       <div className="navbar__icons">
-        <div className="navbar__user">
+        <SearchIcon />
+        <div className="navbar__notification-wrapper">
+          <NotificationsIcon
+            className="navbar__notification-icon"
+            id="navbar__notification-icon"
+            onClick={onClickShowNotificationMenu}
+          />
+          <div
+            className="navbar__notification-modal menu-hidden"
+            id="navbar__notification-modal"
+          >
+            <div className="notification-modal__header">
+              <h3>Notificações</h3>
+            </div>
+            <div className="notification-modal__content">
+              <div className="notification-modal__item">
+                <PaidIcon />
+                <span>
+                  O cliente <strong>Teste</strong> efetuou uma nova compra
+                </span>
+              </div>
+              <div className="notification-modal__item">
+                <PaidIcon />
+                <span>
+                  O cliente <strong>Teste</strong> efetuou uma nova compra
+                </span>
+              </div>
+              <div className="notification-modal__item">
+                <PaidIcon />
+                <span>
+                  O cliente <strong>Teste</strong> efetuou uma nova compra
+                </span>
+              </div>
+              <div className="notification-modal__item">
+                <PaidIcon />
+                <span>
+                  O cliente <strong>Teste</strong> efetuou uma nova compra
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <MenuIcon
+          className="navbar__menu-icon"
+          onClick={onClickShowNavbarMenu}
+        />
+        <div className="navbar__user" onClick={() => navigate("/userProfile")}>
           <Avatar
             alt="Remy Sharp"
             src="https://images.generated.photos/EuuJzFsXBWXS1u0Dg4rDlCl4Kak2k0q__wGguSkfFhM/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzIwNzM0LmpwZw.jpg"
             sx={{ width: 38, height: 38 }}
           />
           <span>Username</span>
+          <ArrowRightIcon fontSize="large" />
         </div>
-        <SearchIcon />
-        <NotificationsIcon />
-        <MenuIcon onClick={onClickShowNavbarMenu} />
       </div>
       <div className="navbar__menu-overlay-hidden">
         <CloseIcon
