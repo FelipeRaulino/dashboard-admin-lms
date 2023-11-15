9dca277295c0f35aadd7f0eaea2a0be0d08d567a
Felipe Raulino <felipe.raaulino29@gmail.com>
2023-11-15 11:46:34 -0300

    feat: adiciona responsividade na navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,11 +3,29 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 import "./Navbar.css";
 import { Avatar } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  function onClickShowNavbarMenu() {
+    const navbarMenu = document.getElementsByClassName(
+      "navbar__menu-overlay-hidden"
+    );
+
+    navbarMenu[0].classList.add("navbar__menu-overlay");
+    navbarMenu[0].classList.remove("navbar__menu-overlay-hidden");
+  }
+
+  function onClickHiddenNavbarMenu() {
+    const navbarMenu = document.getElementsByClassName("navbar__menu-overlay");
+
+    navbarMenu[0].classList.add("navbar__menu-overlay-hidden");
+    navbarMenu[0].classList.remove("navbar__menu-overlay");
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -25,7 +43,47 @@ const Navbar = () => {
         </div>
         <SearchIcon />
         <NotificationsIcon />
-        <MenuIcon />
+        <MenuIcon onClick={onClickShowNavbarMenu} />
+      </div>
+      <div className="navbar__menu-overlay-hidden">
+        <CloseIcon
+          fontSize="large"
+          className="menu__close-btn"
+          onClick={onClickHiddenNavbarMenu}
+        />
+        <div className="menu-modal__header">
+          <DashboardIcon />
+          <h3>Useradmin</h3>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/" onClick={onClickHiddenNavbarMenu}>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" onClick={onClickHiddenNavbarMenu}>
+                Clientes
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/products" onClick={onClickHiddenNavbarMenu}>
+                Produtos
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/sells" onClick={onClickHiddenNavbarMenu}>
+                Vendas
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/userProfile" onClick={onClickHiddenNavbarMenu}>
+                Ver perfil
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </nav>
   );
