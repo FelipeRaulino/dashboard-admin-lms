8e2f5e06a3cfa5fba40b0eee48a03fa827c7b8bc
Felipe Raulino <felipe.raaulino29@gmail.com>
2023-11-26 11:41:32 -0300

    feat: responsividade na tela de produtos

--- src/features/products/routes/Products/Products.jsx
+++ src/features/products/routes/Products/Products.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
 import "./Products.css";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 import { Box } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import StarIcon from '@mui/icons-material/Star';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import StarIcon from "@mui/icons-material/Star";
 
 import { getProducts } from "../../api/getProducts";
 
 const Products = () => {
-
   const [products, setProducts] = React.useState([]);
 
   async function fetchData() {
@@ -25,54 +24,59 @@ const Products = () => {
     fetchData();
   }, []);
 
-  console.log(getProducts())
   return (
     <section className="products">
       <h1>Produtos</h1>
-      {
-      products.length > 0 ? 
-      products.map((product) => {
-        return (
-          <Box sx={{ maxWidth: "98%", marginTop: 2 }}>
-            <Card sx={{ display: 'flex', minHeight: 300 }} key={product.id}>
-              <CardMedia
-                sx={{ 
-                  width: 200, 
-                  height: 300
-                 }}
-                component="img"
-                alt="green iguana"
-                image={product.image}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {product.title} [$ {product.price}]
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Categoria: {product.category}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Estoque: {product.rating.count}
-                </Typography>
-                <Typography variant="body2">
-                  Descrição: {product.description}
-                </Typography>
-              </CardContent>
-              <CardActions sx={{marginRight: 5, textAlign: 'center',}}>
-                <Typography gutterBottom variant="h5" component="div" sx={{width: 100}}>
-                  <StarIcon /> {product.rating.rate}
-                <Button variant="outlined">Comprar</Button>
-                </Typography>
-              </CardActions>
-            </Card>
-          </Box>
-          )
-      })
-      : 
-      <Box sx={{ display: 'flex' }}>
-        <CircularProgress />
-      </Box>
-      }
+      {products.length > 0 ? (
+        <Box sx={{ maxWidth: "90%", marginTop: 2 }}>
+          {products.map((product) => {
+            return (
+              <Box sx={{ maxWidth: "100%", marginTop: 2 }} key={product.id}>
+                <Card sx={{ display: "flex", minHeight: 300 }}>
+                  <CardMedia
+                    sx={{
+                      width: 200,
+                      height: 300,
+                    }}
+                    component="img"
+                    alt="green iguana"
+                    image={product.image}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {product.title} [$ {product.price}]
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Categoria: {product.cat}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Estoque: {product.rating.count}
+                    </Typography>
+                    <Typography variant="body2">
+                      Descrição: {product.desc}
+                    </Typography>
+                  </CardContent>
+                  <CardActions sx={{ marginRight: 5, textAlign: "center" }}>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      sx={{ width: 100 }}
+                    >
+                      <StarIcon /> {product.rating.rate}
+                      <Button variant="outlined">Comprar</Button>
+                    </Typography>
+                  </CardActions>
+                </Card>
+              </Box>
+            );
+          })}
+        </Box>
+      ) : (
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      )}
     </section>
   );
 };
