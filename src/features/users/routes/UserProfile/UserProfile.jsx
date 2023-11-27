a55a47b5f917506e015d1fa5207dc16be9d6bfe3
Italorodri <italorodriguess@alu.ufc.br>
2023-11-27 08:27:57 -0300

    alteracao tela perfil

--- src/features/users/routes/UserProfile/UserProfile.jsx
+++ src/features/users/routes/UserProfile/UserProfile.jsx
@@ -4,8 +4,48 @@ import "./UserProfile.css";
 
 const UserProfile = () => {
   return (
-    <section className="user-profile">
+    <section class="user-profile">
       <h1>Pefil do Usuário</h1>
+      <br />
+      <h1 class="titulo">Formulário de cadastro</h1>
+      <br />
+      <p>
+        <img src="src/assets/imagemPerfil.png"></img>
+      </p>
+      <div class="formulario">
+        <br />
+        <form>
+          <label>Primeiro Nome:</label>
+          <br />
+          <input type="text" size="50" name="primeironome" />
+          <br />
+          <label>Segundo Nome:</label>
+          <br />
+          <input type="text" name="segundonome" />
+          <br />
+          <label>CPF:</label>
+          <br />
+          <input type="number" name="cpf" />
+          <br />
+          <label>Telefone:</label>
+          <br />
+          <input type="number" name="segundonome" />
+          <br />
+          <label>Email:</label>
+          <br />
+          <input type="email" name="email" />
+          <br />
+          <label>Senha:</label>
+          <br />
+          <input type="password" name="senha" />
+          <br />
+          <label>Repetir Senha:</label>
+          <br />
+          <input type="password" name="repetirsenha" />
+          <br />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
     </section>
   );
 };
