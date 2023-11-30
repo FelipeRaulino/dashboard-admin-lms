7590a0a201acc3d00b0856be6dd63c7afd185eaf
WeslliVs <wesllivs@gmail.com>
2023-11-29 22:13:14 -0300

    feat: adição de código css em "Sales"

--- src/features/sales/routes/Sales/Sales.jsx
+++ src/features/sales/routes/Sales/Sales.jsx
@@ -1,66 +1,81 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Backdrop, CircularProgress } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import "./Sales.css";
 import { getAllSales } from "../../utils/salesUtils";
 
 const Sales = () => {
-  const [users, setUsers] = React.useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const columns = [
-    { field: "count", headerName: "Total de Vendas", flex: 0.3 },
+    { field: "count", headerName: "Total de Vendas", flex: 0.5 },
     { field: "title", headerName: "Item", flex: 1 },
-    { field: "category", headerName: "Categoria", flex: 0.3 },
+    { field: "category", headerName: "Categoria", flex: 0.5 },
     { field: "price", headerName: "Preço", flex: 1 },
   ];
 
   async function fetchData() {
-    const usersResponse = await getAllSales();
-    const productNameToIdMap = {};
-    const salesWithId = [];
+    try {
+      const usersResponse = await getAllSales();
+      const productNameToIdMap = {};
+      const salesWithId = [];
 
-    usersResponse.forEach((user) => {
-      const itemName = user.productName;
-      const itemCategory = user.productCategory;
-      const itemPreco = user.productPrice;
+      usersResponse.forEach((user) => {
+        const itemName = user.productName;
+        const itemCategory = user.productCategory;
+        const itemPreco = user.productPrice;
 
-      if (productNameToIdMap[itemName]) {
-        const existingItem = salesWithId.find((item) => item.id === productNameToIdMap[itemName]);
-        if (existingItem) {
-          existingItem.count += 1;
+        if (productNameToIdMap[itemName]) {
+          const existingItem = salesWithId.find(
+            (item) => item.id === productNameToIdMap[itemName]
+          );
+          if (existingItem) {
+            existingItem.count += 1;
+          }
+        } else {
+          const newItemId = Object.keys(productNameToIdMap).length + 1;
+          productNameToIdMap[itemName] = newItemId;
+
+          salesWithId.push({
+            id: newItemId,
+            title: itemName,
+            category: itemCategory,
+            price: itemPreco,
+            count: 1,
+          });
         }
-      } else {
-        const newItemId = Object.keys(productNameToIdMap).length + 1;
-        productNameToIdMap[itemName] = newItemId;
+      });
 
-        salesWithId.push({
-          id: newItemId,
-          title: itemName,
-          category: itemCategory,
-          price: itemPreco,
-          count: 1,
-        });
-      }
-    });
-
-    setUsers(salesWithId);
+      setUsers(salesWithId);
+    } finally {
+      setLoading(false);
+    }
   }
 
   const defaultSortModel = [
     {
-      field: 'count',
-      sort: 'desc',
+      field: "count",
+      sort: "desc",
     },
   ];
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <section className="sales">
       <h1>Sales</h1>
-      <Box sx={{ height: 400, maxWidth: "98%", marginTop: 4 }}>
+      <br/>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Box sx={{ width: "98%", position: "relative" }}>
         <DataGrid
           rows={!!users && users}
           columns={columns}
@@ -69,6 +84,7 @@ const Sales = () => {
             color: "#fff",
             borderColor: "#fff",
             border: 2,
+            transition: "height 0.5s",
           }}
         />
       </Box>
